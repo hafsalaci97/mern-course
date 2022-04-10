6ddfa780bc0bbcd7a90d0f724092786e5c128323
hafsalaci <hafsa.laci18@gmail.com>
2022-04-11 00:38:43 +0200

    Arrays

---
+++ Arrays/todo.js
@@ -0,0 +1,74 @@
+//push front
+// let arr = ['test1', 'test2', 'test3', 'test4', 'test5'];
+// i = arr.length;
+// console.log(arr.length);
+// console.log(arr);
+
+// while (i-- > 0){
+//     arr[i + 1] = arr[i];
+// }
+// arr[0] = 'test0';
+// console.log(arr);
+
+//pop front
+// let arr = ['test0', 'test1', 'test2', 'test3', 'test4', 'test5'];
+// for (let i = 1; i < arr.length; i++) { 
+//     arr[i-1] = arr[i];}
+//     arr.length--;
+// console.log(arr);
+
+//Insert At
+// let arr = ['test1', 'test2', 'test3', 'test4', 'test5'];
+// i = arr.length;
+// pos = 2;
+// console.log(arr.length);
+// console.log(arr);
+
+// while (i-- > pos){
+//     arr[i + 1] = arr[i];
+// }
+// arr[pos] = 'test0';
+// console.log(arr);
+
+//Remove At
+// let arr = ['test0', 'test1', 'test2', 'test3', 'test4', 'test5'];
+// let arr1 = [];
+// pos = 2;
+// for (let i = 0; i < arr.length; i++) {
+//     if (i==pos){  
+//     arr[i] = arr[i+1];
+// }
+// }
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== arr[i+1]){  
+//     arr1.push(arr[i]);
+// }
+// }
+// arr = arr1;
+// console.log(arr);
+
+//Swap Pairs
+// let arr = ['test0', 'test1', 'test2', 'test3', 'test4', 'test5'];
+// console.log(arr.length-1);
+// for (let i=0; i<arr.length-1; i=i+2){
+//         let j = i+1;
+//         let temp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp;
+//     }
+// console.log(arr);
+
+//Remove Duplicates
+// let arr = [1, 2, 2, 2, 3, 3, 4, 4, 5];
+// let arr1 = [];
+
+// for (let i=0; i<arr.length; i++){
+//     if (arr[i] !== arr[i+1]){
+//         arr1.push(arr[i]);
+//     }
+// }
+// arr = arr1;
+
+// console.log(arr);
+
+
