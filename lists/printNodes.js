2dedf5d8c6c84f995a89c54a5ff6ede2b0937e93
hafsalaci <hafsa.laci18@gmail.com>
2022-04-13 23:48:06 +0200

    print all nodes core

--- lists/printNodes.js
+++ lists/printNodes.js
@@ -19,19 +19,6 @@ class SLL {
         this.head = new_node;
         return this;
     }
-    //remove value from the front
-    removeFront() {
-        if (!this.head) return null;
-        let valueRemoved = this.head.val;
-        this.head = this.head.next;
-        return valueRemoved;
-    }
-    //print "value"
-    front(){
-        if (!this.head) return null;
-        return this.head.data;
-    }
-    //print all nodes in the list
     display(){
         let currentNode = this.head;
         while (currentNode != null){
@@ -45,8 +32,5 @@ LL.addFront(9);
 LL.addFront(10);
 LL.addFront(11);
 LL.display();
-LL.removeFront();
-LL.front();
 // console.dir(LL, {depth: 3});
 console.log(LL);
-console.log(LL.front());
